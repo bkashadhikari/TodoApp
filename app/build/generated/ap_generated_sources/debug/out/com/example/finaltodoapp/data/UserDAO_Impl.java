1582d1884dfe2e0aed2149ca30730445668a500c
bkashadhikari <abikash21@tbc.edu.np>
2021-12-28 19:29:01 +0545

    todo app completed

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/finaltodoapp/data/UserDAO_Impl.java
@@ -0,0 +1,234 @@
+package com.example.finaltodoapp.data;
+
+import android.database.Cursor;
+import androidx.lifecycle.LiveData;
+import androidx.room.EntityDeletionOrUpdateAdapter;
+import androidx.room.EntityInsertionAdapter;
+import androidx.room.RoomDatabase;
+import androidx.room.RoomSQLiteQuery;
+import androidx.room.SharedSQLiteStatement;
+import androidx.room.util.CursorUtil;
+import androidx.room.util.DBUtil;
+import androidx.sqlite.db.SupportSQLiteStatement;
+import com.example.finaltodoapp.model.User;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class UserDAO_Impl implements UserDAO {
+  private final RoomDatabase __db;
+
+  private final EntityInsertionAdapter<User> __insertionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __deletionAdapterOfUser;
+
+  private final EntityDeletionOrUpdateAdapter<User> __updateAdapterOfUser;
+
+  private final SharedSQLiteStatement __preparedStmtOfDeleteAll;
+
+  public UserDAO_Impl(RoomDatabase __db) {
+    this.__db = __db;
+    this.__insertionAdapterOfUser = new EntityInsertionAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "INSERT OR ABORT INTO `user_table` (`id`,`username`,`email`,`password`) VALUES (nullif(?, 0),?,?,?)";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getUsername() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getUsername());
+        }
+        if (value.getEmail() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getEmail());
+        }
+        if (value.getPassword() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getPassword());
+        }
+      }
+    };
+    this.__deletionAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "DELETE FROM `user_table` WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        stmt.bindLong(1, value.getId());
+      }
+    };
+    this.__updateAdapterOfUser = new EntityDeletionOrUpdateAdapter<User>(__db) {
+      @Override
+      public String createQuery() {
+        return "UPDATE OR REPLACE `user_table` SET `id` = ?,`username` = ?,`email` = ?,`password` = ? WHERE `id` = ?";
+      }
+
+      @Override
+      public void bind(SupportSQLiteStatement stmt, User value) {
+        stmt.bindLong(1, value.getId());
+        if (value.getUsername() == null) {
+          stmt.bindNull(2);
+        } else {
+          stmt.bindString(2, value.getUsername());
+        }
+        if (value.getEmail() == null) {
+          stmt.bindNull(3);
+        } else {
+          stmt.bindString(3, value.getEmail());
+        }
+        if (value.getPassword() == null) {
+          stmt.bindNull(4);
+        } else {
+          stmt.bindString(4, value.getPassword());
+        }
+        stmt.bindLong(5, value.getId());
+      }
+    };
+    this.__preparedStmtOfDeleteAll = new SharedSQLiteStatement(__db) {
+      @Override
+      public String createQuery() {
+        final String _query = "DELETE FROM user_table";
+        return _query;
+      }
+    };
+  }
+
+  @Override
+  public void insert(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __insertionAdapterOfUser.insert(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void deleteById(final User user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __deletionAdapterOfUser.handle(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void update(final User... user) {
+    __db.assertNotSuspendingTransaction();
+    __db.beginTransaction();
+    try {
+      __updateAdapterOfUser.handleMultiple(user);
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+    }
+  }
+
+  @Override
+  public void deleteAll() {
+    __db.assertNotSuspendingTransaction();
+    final SupportSQLiteStatement _stmt = __preparedStmtOfDeleteAll.acquire();
+    __db.beginTransaction();
+    try {
+      _stmt.executeUpdateDelete();
+      __db.setTransactionSuccessful();
+    } finally {
+      __db.endTransaction();
+      __preparedStmtOfDeleteAll.release(_stmt);
+    }
+  }
+
+  @Override
+  public User getUserById(final int id) {
+    final String _sql = "SELECT * FROM user_table WHERE id=?";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 1);
+    int _argIndex = 1;
+    _statement.bindLong(_argIndex, id);
+    __db.assertNotSuspendingTransaction();
+    final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+    try {
+      final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+      final int _cursorIndexOfUsername = CursorUtil.getColumnIndexOrThrow(_cursor, "username");
+      final int _cursorIndexOfEmail = CursorUtil.getColumnIndexOrThrow(_cursor, "email");
+      final int _cursorIndexOfPassword = CursorUtil.getColumnIndexOrThrow(_cursor, "password");
+      final User _result;
+      if(_cursor.moveToFirst()) {
+        final String _tmpUsername;
+        _tmpUsername = _cursor.getString(_cursorIndexOfUsername);
+        final String _tmpEmail;
+        _tmpEmail = _cursor.getString(_cursorIndexOfEmail);
+        final String _tmpPassword;
+        _tmpPassword = _cursor.getString(_cursorIndexOfPassword);
+        _result = new User(_tmpUsername,_tmpEmail,_tmpPassword);
+        final int _tmpId;
+        _tmpId = _cursor.getInt(_cursorIndexOfId);
+        _result.setId(_tmpId);
+      } else {
+        _result = null;
+      }
+      return _result;
+    } finally {
+      _cursor.close();
+      _statement.release();
+    }
+  }
+
+  @Override
+  public LiveData<List<User>> getAllUsers() {
+    final String _sql = "SELECT * FROM user_table";
+    final RoomSQLiteQuery _statement = RoomSQLiteQuery.acquire(_sql, 0);
+    return __db.getInvalidationTracker().createLiveData(new String[]{"user_table"}, false, new Callable<List<User>>() {
+      @Override
+      public List<User> call() throws Exception {
+        final Cursor _cursor = DBUtil.query(__db, _statement, false, null);
+        try {
+          final int _cursorIndexOfId = CursorUtil.getColumnIndexOrThrow(_cursor, "id");
+          final int _cursorIndexOfUsername = CursorUtil.getColumnIndexOrThrow(_cursor, "username");
+          final int _cursorIndexOfEmail = CursorUtil.getColumnIndexOrThrow(_cursor, "email");
+          final int _cursorIndexOfPassword = CursorUtil.getColumnIndexOrThrow(_cursor, "password");
+          final List<User> _result = new ArrayList<User>(_cursor.getCount());
+          while(_cursor.moveToNext()) {
+            final User _item;
+            final String _tmpUsername;
+            _tmpUsername = _cursor.getString(_cursorIndexOfUsername);
+            final String _tmpEmail;
+            _tmpEmail = _cursor.getString(_cursorIndexOfEmail);
+            final String _tmpPassword;
+            _tmpPassword = _cursor.getString(_cursorIndexOfPassword);
+            _item = new User(_tmpUsername,_tmpEmail,_tmpPassword);
+            final int _tmpId;
+            _tmpId = _cursor.getInt(_cursorIndexOfId);
+            _item.setId(_tmpId);
+            _result.add(_item);
+          }
+          return _result;
+        } finally {
+          _cursor.close();
+        }
+      }
+
+      @Override
+      protected void finalize() {
+        _statement.release();
+      }
+    });
+  }
+}
---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/finaltodoapp/model/TodoRoomDatabase_Impl.java
@@ -0,0 +1,180 @@
+package com.example.finaltodoapp.model;
+
+import androidx.room.DatabaseConfiguration;
+import androidx.room.InvalidationTracker;
+import androidx.room.RoomOpenHelper;
+import androidx.room.RoomOpenHelper.Delegate;
+import androidx.room.RoomOpenHelper.ValidationResult;
+import androidx.room.util.DBUtil;
+import androidx.room.util.TableInfo;
+import androidx.room.util.TableInfo.Column;
+import androidx.room.util.TableInfo.ForeignKey;
+import androidx.room.util.TableInfo.Index;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+import androidx.sqlite.db.SupportSQLiteOpenHelper;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Callback;
+import androidx.sqlite.db.SupportSQLiteOpenHelper.Configuration;
+import com.example.finaltodoapp.data.TodoDAO;
+import com.example.finaltodoapp.data.TodoDAO_Impl;
+import com.example.finaltodoapp.data.UserDAO;
+import com.example.finaltodoapp.data.UserDAO_Impl;
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+@SuppressWarnings({"unchecked", "deprecation"})
+public final class TodoRoomDatabase_Impl extends TodoRoomDatabase {
+  private volatile TodoDAO _todoDAO;
+
+  private volatile UserDAO _userDAO;
+
+  @Override
+  protected SupportSQLiteOpenHelper createOpenHelper(DatabaseConfiguration configuration) {
+    final SupportSQLiteOpenHelper.Callback _openCallback = new RoomOpenHelper(configuration, new RoomOpenHelper.Delegate(2) {
+      @Override
+      public void createAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("CREATE TABLE IF NOT EXISTS `user_table` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `username` TEXT, `email` TEXT, `password` TEXT)");
+        _db.execSQL("CREATE TABLE IF NOT EXISTS `todo_table` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `title` TEXT NOT NULL, `description` TEXT, `todo_date` INTEGER, `priority` INTEGER NOT NULL, `is_completed` INTEGER NOT NULL)");
+        _db.execSQL("CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)");
+        _db.execSQL("INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, 'd792572f4b46b79fb4df92c2f9473717')");
+      }
+
+      @Override
+      public void dropAllTables(SupportSQLiteDatabase _db) {
+        _db.execSQL("DROP TABLE IF EXISTS `user_table`");
+        _db.execSQL("DROP TABLE IF EXISTS `todo_table`");
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onDestructiveMigration(_db);
+          }
+        }
+      }
+
+      @Override
+      protected void onCreate(SupportSQLiteDatabase _db) {
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onCreate(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onOpen(SupportSQLiteDatabase _db) {
+        mDatabase = _db;
+        internalInitInvalidationTracker(_db);
+        if (mCallbacks != null) {
+          for (int _i = 0, _size = mCallbacks.size(); _i < _size; _i++) {
+            mCallbacks.get(_i).onOpen(_db);
+          }
+        }
+      }
+
+      @Override
+      public void onPreMigrate(SupportSQLiteDatabase _db) {
+        DBUtil.dropFtsSyncTriggers(_db);
+      }
+
+      @Override
+      public void onPostMigrate(SupportSQLiteDatabase _db) {
+      }
+
+      @Override
+      protected RoomOpenHelper.ValidationResult onValidateSchema(SupportSQLiteDatabase _db) {
+        final HashMap<String, TableInfo.Column> _columnsUserTable = new HashMap<String, TableInfo.Column>(4);
+        _columnsUserTable.put("id", new TableInfo.Column("id", "INTEGER", true, 1, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsUserTable.put("username", new TableInfo.Column("username", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsUserTable.put("email", new TableInfo.Column("email", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsUserTable.put("password", new TableInfo.Column("password", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        final HashSet<TableInfo.ForeignKey> _foreignKeysUserTable = new HashSet<TableInfo.ForeignKey>(0);
+        final HashSet<TableInfo.Index> _indicesUserTable = new HashSet<TableInfo.Index>(0);
+        final TableInfo _infoUserTable = new TableInfo("user_table", _columnsUserTable, _foreignKeysUserTable, _indicesUserTable);
+        final TableInfo _existingUserTable = TableInfo.read(_db, "user_table");
+        if (! _infoUserTable.equals(_existingUserTable)) {
+          return new RoomOpenHelper.ValidationResult(false, "user_table(com.example.finaltodoapp.model.User).\n"
+                  + " Expected:\n" + _infoUserTable + "\n"
+                  + " Found:\n" + _existingUserTable);
+        }
+        final HashMap<String, TableInfo.Column> _columnsTodoTable = new HashMap<String, TableInfo.Column>(6);
+        _columnsTodoTable.put("id", new TableInfo.Column("id", "INTEGER", true, 1, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsTodoTable.put("title", new TableInfo.Column("title", "TEXT", true, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsTodoTable.put("description", new TableInfo.Column("description", "TEXT", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsTodoTable.put("todo_date", new TableInfo.Column("todo_date", "INTEGER", false, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsTodoTable.put("priority", new TableInfo.Column("priority", "INTEGER", true, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        _columnsTodoTable.put("is_completed", new TableInfo.Column("is_completed", "INTEGER", true, 0, null, TableInfo.CREATED_FROM_ENTITY));
+        final HashSet<TableInfo.ForeignKey> _foreignKeysTodoTable = new HashSet<TableInfo.ForeignKey>(0);
+        final HashSet<TableInfo.Index> _indicesTodoTable = new HashSet<TableInfo.Index>(0);
+        final TableInfo _infoTodoTable = new TableInfo("todo_table", _columnsTodoTable, _foreignKeysTodoTable, _indicesTodoTable);
+        final TableInfo _existingTodoTable = TableInfo.read(_db, "todo_table");
+        if (! _infoTodoTable.equals(_existingTodoTable)) {
+          return new RoomOpenHelper.ValidationResult(false, "todo_table(com.example.finaltodoapp.model.ETodo).\n"
+                  + " Expected:\n" + _infoTodoTable + "\n"
+                  + " Found:\n" + _existingTodoTable);
+        }
+        return new RoomOpenHelper.ValidationResult(true, null);
+      }
+    }, "d792572f4b46b79fb4df92c2f9473717", "bbd808763a025afc7ab42b64ae0648ca");
+    final SupportSQLiteOpenHelper.Configuration _sqliteConfig = SupportSQLiteOpenHelper.Configuration.builder(configuration.context)
+        .name(configuration.name)
+        .callback(_openCallback)
+        .build();
+    final SupportSQLiteOpenHelper _helper = configuration.sqliteOpenHelperFactory.create(_sqliteConfig);
+    return _helper;
+  }
+
+  @Override
+  protected InvalidationTracker createInvalidationTracker() {
+    final HashMap<String, String> _shadowTablesMap = new HashMap<String, String>(0);
+    HashMap<String, Set<String>> _viewTables = new HashMap<String, Set<String>>(0);
+    return new InvalidationTracker(this, _shadowTablesMap, _viewTables, "user_table","todo_table");
+  }
+
+  @Override
+  public void clearAllTables() {
+    super.assertNotMainThread();
+    final SupportSQLiteDatabase _db = super.getOpenHelper().getWritableDatabase();
+    try {
+      super.beginTransaction();
+      _db.execSQL("DELETE FROM `user_table`");
+      _db.execSQL("DELETE FROM `todo_table`");
+      super.setTransactionSuccessful();
+    } finally {
+      super.endTransaction();
+      _db.query("PRAGMA wal_checkpoint(FULL)").close();
+      if (!_db.inTransaction()) {
+        _db.execSQL("VACUUM");
+      }
+    }
+  }
+
+  @Override
+  public TodoDAO mTodoDao() {
+    if (_todoDAO != null) {
+      return _todoDAO;
+    } else {
+      synchronized(this) {
+        if(_todoDAO == null) {
+          _todoDAO = new TodoDAO_Impl(this);
+        }
+        return _todoDAO;
+      }
+    }
+  }
+
+  @Override
+  public UserDAO mUserDao() {
+    if (_userDAO != null) {
+      return _userDAO;
+    } else {
+      synchronized(this) {
+        if(_userDAO == null) {
+          _userDAO = new UserDAO_Impl(this);
+        }
+        return _userDAO;
+      }
+    }
+  }
+}
